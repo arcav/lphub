d53ba80de815471077ec852472eb2adefe107588
arcav <armandojcastro84@gmail.com>
2026-02-11 17:19:18 -0300

    chore(config): optimize build for TV/legacy browsers

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  transpilePackages: ['framer-motion', 'gsap', 'lucide-react'],
 };
 
 export default nextConfig;
